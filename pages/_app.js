8f244d570fd5e87dd260547c880e212c1fee979b
Macguire Rintoul <macguire.rintoul@protonmail.ch>
2021-11-14 11:12:49 -0800

    protocol?

--- pages/_app.js
+++ pages/_app.js
@@ -5,7 +5,7 @@ export default function App({ Component, pageProps }) {
 	return (
 		<PlausibleProvider
 			domain="macguire.me"
-			customDomain="plausible.macguire.me"
+			customDomain="https://plausible.macguire.me"
 			trackOutboundLinks={true}
 			selfHosted={true}
 		>
