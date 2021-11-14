41d3bab260b3fee05cb349552bff5621d0f68e8b
Macguire Rintoul <macguire.rintoul@protonmail.ch>
2021-11-14 11:00:27 -0800

    plausible

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
-import '../styles/style.scss'
+import PlausibleProvider from "next-plausible";
+import "../styles/style.scss";
 
 export default function App({ Component, pageProps }) {
-  return (
-	<Component {...pageProps} /> 
-	)
+	return (
+		<PlausibleProvider
+			domain="macguire.me"
+			customDomain="plausible.macguire.me"
+			trackOutboundLinks={true}
+			selfHosted={true}
+		>
+			<Component {...pageProps} />
+		</PlausibleProvider>
+	);
 }
