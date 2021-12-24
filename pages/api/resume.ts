c0a6fe84b9b2da112b9417987946551d5e5fa39f
Macguire Rintoul <macguire.rintoul@protonmail.ch>
2021-12-24 15:28:26 -0800

    fix

--- pages/api/resume.ts
+++ pages/api/resume.ts
@@ -1,5 +1,31 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import puppeteer from "puppeteer-core";
 import chrome from "chrome-aws-lambda";
+
+const exePath =
+	process.platform === "win32"
+		? "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe"
+		: process.platform === "linux"
+		? "/usr/bin/google-chrome"
+		: "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome";
+
+async function getOptions(isDev: boolean) {
+	let options;
+	if (isDev) {
+		options = {
+			args: [],
+			executablePath: exePath,
+			headless: true,
+		};
+	} else {
+		options = {
+			args: chrome.args,
+			executablePath: await chrome.executablePath,
+			headless: chrome.headless,
+		};
+	}
+	return options;
+}
 
 export default async function handler(
 	req: NextApiRequest,
@@ -12,9 +38,8 @@ export default async function handler(
 	const resumePath = "utility/resume";
 	const url = domain + resumePath;
 
-	const browser = await chrome.puppeteer.launch({
-		headless: true,
-	});
+	const options = await getOptions(process.env.NODE_ENV === "development");
+	const browser = await puppeteer.launch(options);
 	const page = await browser.newPage();
 	await page.goto(url, { waitUntil: "networkidle0" });
 	const pdf = await page.pdf();
