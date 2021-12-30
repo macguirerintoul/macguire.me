9ce4b785fe798061e7d0c8ac45c8e5cf0886f994
Macguire Rintoul <macguire.rintoul@protonmail.ch>
2021-12-30 11:53:13 -0800

    next image

--- pages/visier/[visierProject].tsx
+++ pages/visier/[visierProject].tsx
@@ -4,7 +4,13 @@ import { IProjectParams, IProject } from "../../lib/types";
 import { getProjectData, getVisierWorkIds } from "../../lib/content";
 import { GetStaticProps } from "next";
 
-import Visier from "../../content/images/Visier.png";
+import sourcingPaths from "../../content/images/visier/sourcing-paths.png";
+import scatterPlot from "../../content/images/visier/scatter-plot.png";
+
+const featuredImages: Record<string, StaticImageData> = {
+	"visier/sourcing-paths": sourcingPaths,
+	"visier/scatter-plot": scatterPlot,
+};
 
 export async function getStaticPaths() {
 	const paths = getVisierWorkIds();
@@ -27,7 +33,12 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
 
 class VisierProject extends React.Component<{ projectData: IProject }> {
 	render() {
-		return <ProjectContent project={this.props.projectData} imgSrc={Visier} />;
+		return (
+			<ProjectContent
+				project={this.props.projectData}
+				imgSrc={featuredImages[this.props.projectData.id]}
+			/>
+		);
 	}
 }
 
