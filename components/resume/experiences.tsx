4b13ca1653a1c8bfda95c33c0f38ca7c31214180
Macguire Rintoul <macguire.rintoul@protonmail.ch>
2022-06-10 23:01:28 -0700

    r

--- components/resume/experiences.tsx
+++ components/resume/experiences.tsx
@@ -22,11 +22,24 @@ class Experiences extends React.Component<IProps> {
 		this.lengthOfService = this.lengthOfService.bind(this);
 	}
 
-	lengthOfService = (start: Date, end: Date): string => {
+	lengthOfService = (
+		start: IResumeTime["start"],
+		end: IResumeTime["end"]
+	): string => {
+		const startDate = new Date(start.year, start.month - 1);
+		const startString = startDate.toLocaleString(undefined, {
+			month: "long",
+			year: "numeric",
+		});
+		let endDate: Date = new Date();
+		if (typeof end !== "undefined" && end["year"] && end["month"]) {
+			endDate = new Date(end.year, end.month - 1);
+		}
 		const lenghInMonths: number =
-			end.getMonth() -
-			start.getMonth() +
-			12 * (end.getFullYear() - start.getFullYear()) + 1;
+			endDate.getMonth() -
+			startDate.getMonth() +
+			12 * (endDate.getFullYear() - startDate.getFullYear()) +
+			1;
 		const years: number = Math.floor(lenghInMonths / 12);
 		const months: number = lenghInMonths % 12;
 		const yearString = `${years} ${years > 1 ? "years" : "year"}`;
@@ -36,6 +49,26 @@ class Experiences extends React.Component<IProps> {
 		} else {
 			return `${yearString} ${monthString}`;
 		}
+	};
+
+	getCalendarString = (
+		start: IResumeTime["start"],
+		end: IResumeTime["end"]
+	) => {
+		const startDate = new Date(start.year, start.month - 1);
+		const startString = startDate.toLocaleString(undefined, {
+			month: "long",
+			year: "numeric",
+		});
+		let endDate: Date = new Date();
+		if (typeof end !== "undefined" && end["year"] && end["month"]) {
+			endDate = new Date(end.year, end.month - 1);
+		}
+		const endString: string =
+			typeof end === "undefined"
+				? "present"
+				: endDate.toLocaleString(undefined, { month: "long", year: "numeric" });
+		return `${startString} – ${endString}`;
 	};
 
 	getTimeString = (start: IResumeTime["start"], end: IResumeTime["end"]) => {
@@ -52,7 +85,7 @@ class Experiences extends React.Component<IProps> {
 			typeof end === "undefined"
 				? "present"
 				: endDate.toLocaleString(undefined, { month: "long", year: "numeric" });
-		const lengthOfService: string = this.lengthOfService(startDate, endDate);
+		const lengthOfService: string = this.lengthOfService(start, end);
 		const timeString = `${lengthOfService} (${startString} – ${endString})`;
 		return timeString;
 	};
@@ -62,23 +95,21 @@ class Experiences extends React.Component<IProps> {
 			return (
 				<div key={job.organization} className="resume-item">
 					<h3>
-						{job.organization} • {this.getTimeString(job.start, job.end)}
+						{job.organization} • {this.lengthOfService(job.start, job.end)}
 					</h3>
-					<h4 className="resume-subtitle">{job.job}</h4>
-					{job.description &&
-						job.description.map((item) => {
-							return (
-								<p className="resume-responsibilities" key={item}>
-									{item}
-								</p>
-							);
-						})}
 					{job.history &&
 						job.history.map((item) => {
 							return (
-								<h4 className="resume-subtitle" key={item}>
-									{item.job} • {this.getTimeString(item.start, item.end)}
-								</h4>
+								<div key={item}>
+									<h4 className="resume-subtitle" key={item}>
+										{item.job} • {this.getCalendarString(item.start, item.end)}
+									</h4>
+									{item.description && (
+										<p className="resume-responsibilities" key={item}>
+											{item.description}
+										</p>
+									)}
+								</div>
 							);
 						})}
 				</div>
