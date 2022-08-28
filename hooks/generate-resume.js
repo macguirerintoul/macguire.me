a691a574974dc02586575decefabafe1060b766d
Macguire Rintoul <macguire.rintoul@protonmail.ch>
2022-08-28 12:04:19 -0700

    public

--- hooks/generate-resume.js
+++ hooks/generate-resume.js
@@ -1,13 +1,16 @@
-const http = require('http'); // or 'https' for https:// URLs
-const fs = require('fs');
+const http = require("http"); // or 'https' for https:// URLs
+const fs = require("fs");
 
-const file = fs.createWriteStream("./Macguire Rintoul - Resume.pdf");
-const request = http.get("http://localhost:3000/api/resume", function(response) {
-   response.pipe(file);
+const file = fs.createWriteStream("./public/Macguire Rintoul - Resume.pdf");
+const request = http.get(
+	"http://localhost:3000/api/resume",
+	function (response) {
+		response.pipe(file);
 
-   // after download completed close filestream
-   file.on("finish", () => {
-       file.close();
-       console.log("Download Completed");
-   });
-});
+		// after download completed close filestream
+		file.on("finish", () => {
+			file.close();
+			console.log("Download Completed");
+		});
+	}
+);
