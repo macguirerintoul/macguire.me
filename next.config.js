e2baf5f140f13d0b6ee5b8936fa0931df77eedd0
Macguire Rintoul <git@macguire.me>
2023-05-05 21:05:30 -0700

    add next bundle analyzer

--- next.config.js
+++ next.config.js
@@ -1,14 +1,6 @@
-// TODO i think this can be deleted since i'm not creating direct mdx pages
-
-const withMDX = require("@next/mdx")({
-	extension: /\.mdx?$/,
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+	enabled: process.env.ANALYZE === "true",
 });
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-	pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"],
-	reactStrictMode: true,
-};
-
-// Merge MDX config with Next.js config
-module.exports = withMDX(nextConfig);
+module.exports = withBundleAnalyzer({});
