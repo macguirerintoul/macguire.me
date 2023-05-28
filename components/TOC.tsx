88ef115e812397be8b77777210e14668c13cd1fc
Macguire Rintoul <git@macguire.me>
2023-05-27 23:17:27 -0700

    toc working again

--- components/TOC.tsx
+++ components/TOC.tsx
@@ -2,17 +2,23 @@
 import { Toc } from "@stefanprobst/rehype-extract-toc";
 import { useHeadsObserver } from "lib/clientutils";
 const TOC = ({ headings }: { headings: Toc }) => {
-	const { activeId } = useHeadsObserver();
+	const activeIds = useHeadsObserver();
+	const idArray = activeIds.map((item) => item.target.id);
 	return (
 		<aside className="toc">
 			<p>Contents</p>
+
 			<ul className="util-unstyled-list">
 				{headings.map((item, index) => {
 					return (
 						<li key={index}>
 							<a
 								href={`#${item.id}`}
-								className={item.id === activeId ? "toc-active" : undefined}
+								className={
+									item.id && idArray.includes(item.id)
+										? "toc-active"
+										: undefined
+								}
 							>
 								{item.value}
 							</a>
--- lib/clientUtils.ts
+++ lib/clientUtils.ts
@@ -2,27 +2,34 @@ import { useRef, useState, useEffect } from "react";
 
 function useHeadsObserver() {
 	const observer = useRef();
-	const [activeId, setActiveId] = useState("");
+	const [activeIds, setActiveIds] = useState([]);
+	const headingElementsRef = useRef({});
 
 	useEffect(() => {
-		const handleObsever = (entries) => {
-			entries.forEach((entry) => {
-				if (entry?.isIntersecting) {
-					setActiveId(entry.target.id);
-				}
+		const handleObsever = (headings) => {
+			headingElementsRef.current = headings.reduce((map, headingElement) => {
+				map[headingElement.target.id] = headingElement;
+				return map;
+			}, headingElementsRef.current);
+
+			const visibleHeadings = [];
+			Object.keys(headingElementsRef.current).forEach((key) => {
+				const headingElement = headingElementsRef.current[key];
+				if (headingElement.isIntersecting) visibleHeadings.push(headingElement);
 			});
+			setActiveIds(visibleHeadings);
 		};
 
 		observer.current = new IntersectionObserver(handleObsever, {
-			rootMargin: "-20% 0% -35% 0px",
+			rootMargin: "0px 0px -40% 0px",
 		});
 
-		const elements = document.querySelectorAll("h2, h3", "h4");
+		const elements = document.querySelectorAll("h2, h3, h4, h5, h6");
 		elements.forEach((elem) => observer.current.observe(elem));
 		return () => observer.current?.disconnect();
 	}, []);
 
-	return { activeId };
+	return activeIds;
 }
 
 export { useHeadsObserver };
