b98d6862f9c46be843ee9ba5edf9fc0400fe0eb5
Macguire Rintoul <git@macguire.me>
2023-08-08 19:49:48 -0700

    add album titles

--- components/Albums.tsx
+++ components/Albums.tsx
@@ -7,24 +7,29 @@ const Albums = ({ albums }: { albums: Album[] }) => {
 		<div className="mx-auto flex max-w-6xl justify-center gap-2 py-12">
 			{albums.map(async (album, index) => {
 				return (
-					<MagicLink
-						key={album.title}
-						url={album.url}
-						className="transition-transform  duration-100 after:hidden motion-safe:animate-floatUpFast"
-						style={{ "--animation-order": index } as React.CSSProperties}
-					>
-						<figure className="mx-2 flex max-w-sm flex-col overflow-hidden rounded shadow-lg">
-							<Image
-								alt={album.title}
-								src={album.image}
-								width={200}
-								height={200}
-								placeholder="blur"
-								className=" rounded "
-								blurDataURL={album.blurDataURL}
-							/>
+					<div key={album.title} className="mx-2 flex max-w-sm flex-col ">
+						<figure>
+							<MagicLink
+								url={album.url}
+								className="transition-transform duration-100 after:hidden  motion-safe:animate-floatUpFast"
+								style={{ "--animation-order": index } as React.CSSProperties}
+							>
+								<Image
+									alt={album.title}
+									src={album.image}
+									width={200}
+									height={200}
+									placeholder="blur"
+									className="rounded shadow-lg"
+									blurDataURL={album.blurDataURL}
+								/>
+							</MagicLink>
+							<figcaption className="text-xl text-gray-600">
+								<p className="mb-0 mt-2 font-medium">{album.artist}</p>
+								<p>{album.title}</p>
+							</figcaption>
 						</figure>
-					</MagicLink>
+					</div>
 				);
 			})}
 		</div>
