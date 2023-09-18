a86c432efbe7c2389556cbae64d0bdf4c9d76793
Macguire Rintoul <git@macguire.me>
2023-09-17 19:35:01 -0700

    full response

--- components/Photo.tsx
+++ components/Photo.tsx
@@ -36,17 +36,22 @@ const Photo = (props: {
 				<figcaption className="shrink-0 basis-full md:basis-1/3">
 					{props.caption}
 				</figcaption>
-				<div className="grid shrink-0 basis-full grid-cols-2 grid-rows-3 flex-wrap items-start justify-between justify-around font-mono font-light text-gray-500 md:basis-2/3  md:text-right">
+				<div className="grid shrink-0 basis-full grid-cols-2 grid-rows-3 flex-wrap items-start justify-between font-mono font-light text-gray-500 sm:grid-cols-3 md:basis-2/3 md:text-right">
 					<div>{toMonthString(props.timestamp)}</div>
-					<div className="col-start-1">{props.camera}</div>
-					<div className="col-start-1" title="35 mm film equivalent">
+					<div className="col-start-1 sm:row-start-2">{props.camera}</div>
+					<div
+						className="col-start-1 sm:col-start-2 sm:row-start-1"
+						title="35 mm film equivalent"
+					>
 						~{props.focalLengthIn35mmFormat}
 					</div>
-					<div className="col-start-2 row-start-1 text-right">{props.iso}</div>
-					<div className="col-start-2 row-start-2 text-right">
+					<div className="col-start-2 row-start-1 text-right sm:row-start-2 sm:text-left md:text-right">
+						{props.iso}
+					</div>
+					<div className="col-start-2 row-start-2 text-right sm:col-start-3 sm:row-start-1 sm:text-left md:text-right ">
 						{props.aperture}
 					</div>
-					<div className="col-start-2 row-start-3 text-right">
+					<div className="col-start-2 row-start-3 text-right sm:col-start-3 sm:row-start-2 sm:text-left md:text-right ">
 						{props.shutterSpeed}
 					</div>
 				</div>
