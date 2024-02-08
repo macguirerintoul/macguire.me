c7da06c5b41c658e356937563641b1b746484295
Macguire Rintoul <git@macguire.me>
2024-02-08 13:18:36 -0800

    pendo

--- app/(regular)/layout.tsx
+++ app/(regular)/layout.tsx
@@ -7,6 +7,7 @@ import "react-medium-image-zoom/dist/styles.css";
 import { getLatestCommit } from "lib/utilities";
 import "styles/globals.css";
 import { timesNow } from "lib/utilities";
+import Script from "next/script";
 
 export const metadata: Metadata = {
 	icons: {
@@ -23,6 +24,17 @@ export default async function RootLayout({
 
 	return (
 		<html lang="en" className={timesNow.variable}>
+			<Script id="pendo">
+				{`
+					(function(apiKey){
+						(function(p,e,n,d,o){var v,w,x,y,z;o=p[d]=p[d]||{};o._q=o._q||[];
+						v=['initialize','identify','updateOptions','pageLoad','track'];for(w=0,x=v.length;w<x;++w)(function(m){
+								o[m]=o[m]||function(){o._q[m===v[0]?'unshift':'push']([m].concat([].slice.call(arguments,0)));};})(v[w]);
+								y=e.createElement(n);y.async=!0;y.src='https://cdn.pendo.io/agent/static/'+apiKey+'/pendo.js';
+								z=e.getElementsByTagName(n)[0];z.parentNode.insertBefore(y,z);})(window,document,'script','pendo');
+				})('6c796417-9818-4ce4-54f4-8248b48cdb56');
+				`}
+			</Script>
 			<PlausibleProvider
 				domain="macguire.me"
 				customDomain="https://plausible.macguire.me"
