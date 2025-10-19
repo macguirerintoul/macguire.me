b8da4f73e5317fb001d9f38d27a1971eb5346ecc
Macguire Rintoul <git@macguire.me>
2025-10-19 16:11:20 -0700

    link

--- components/FancyListLink.tsx
+++ components/FancyListLink.tsx
@@ -14,7 +14,7 @@ export const FancyListLink = (props: {
 			className="mb-2 flex justify-between gap-4 rounded-md border border-neutral-200 bg-white p-4 text-black no-underline shadow-xs duration-75 hover:bg-neutral-100 dark:border-neutral-800 dark:bg-neutral-900 dark:text-neutral-100 hover:dark:bg-neutral-950 hover:dark:text-neutral-100"
 		>
 			<div
-				className={`flex shrink gap-2 truncate ${props.isLoading && "h-6 w-64 animate-pulse rounded bg-neutral-200"}`}
+				className={`flex shrink gap-2 truncate ${props.isLoading && "h-6 w-64 animate-pulse rounded bg-neutral-200 dark:bg-neutral-700"}`}
 			>
 				{/* animating this probably isnt working because the array rendering them changes */}
 				<span className="animate-in fade-in">
