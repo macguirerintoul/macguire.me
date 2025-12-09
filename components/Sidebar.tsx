6d3c58414252b40b5c8e19b7d362a8d48460b8ab
Macguire Rintoul <git@macguire.me>
2025-12-08 19:26:03 -0800

    next

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -44,7 +44,7 @@ export const Sidebar = (props: { commit: Commit | string }) => {
 	const pathname = usePathname();
 
 	return (
-		<aside className="sticky top-0 flex h-screen w-3xs shrink-0 flex-col justify-between border-r border-neutral-200 bg-neutral-50 dark:border-neutral-900 dark:bg-neutral-950">
+		<aside className="sticky top-0 flex h-screen w-3xs shrink-0 flex-col justify-between border-r border-neutral-200 bg-neutral-100 dark:border-neutral-900 dark:bg-neutral-950">
 			<nav>
 				<div className="mt-4 mr-0 mb-2 ml-6 text-xl font-semibold">
 					macguire.me
--- components/SidebarItem.tsx
+++ components/SidebarItem.tsx
@@ -5,7 +5,7 @@ interface SidebarItemProps {
 	href: string;
 	label: string;
 	icon: ReactNode;
-	isActive?: boolean;
+	isActive: boolean;
 }
 
 export const SidebarItem = ({
@@ -18,7 +18,7 @@ export const SidebarItem = ({
 		<li className="m-0">
 			<MagicLink
 				href={href}
-				className={`flex items-center gap-3 rounded-md p-2 px-4 text-base font-medium text-neutral-700 no-underline transition-colors hover:bg-neutral-100 hover:text-black dark:text-neutral-200 dark:hover:bg-neutral-900 dark:hover:text-white ${isActive ? "bg-white text-black shadow-xs ring ring-neutral-100 hover:bg-white dark:bg-neutral-900 dark:ring-neutral-800" : ""}`}
+				className={`flex items-center gap-3 rounded-md p-2 px-4 text-base font-medium no-underline hover:bg-white hover:text-black dark:hover:bg-neutral-900 dark:hover:text-white ${isActive ? "bg-white text-black shadow-xs ring ring-neutral-200 hover:bg-white dark:bg-neutral-900 dark:text-white dark:ring-neutral-800" : "text-neutral-700 dark:text-neutral-200"}`}
 			>
 				{icon}
 				<span>{label}</span>
